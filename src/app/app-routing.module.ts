c4df97152ca7f35b78cd75ca2c261623bd8b5452
Paulo Tenório <paulorltenorio@gmail.com>
2025-03-31 11:24:39 -0300

    feat: criação do componente Modais e configuração de rotas
    - Gerado componente Modais
    - Configurada rota principal ('') para exibir o ModaisComponent
    - Atualizado app-routing.module.ts com a rota
    - Atualizado app.module.ts com importações e declarações necessárias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModaisComponent } from './modais/modais.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ModaisComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modais/modais.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modais',
+  standalone: false,
+  templateUrl: './modais.component.html',
+  styleUrl: './modais.component.css'
+})
+export class ModaisComponent {
+
+}
